eb2cbddc17028651965103b483dfab2041666208
mtlabdo <abdessamadmaatallaoui@gmail.com>
2025-08-01 10:23:43 +0200

    Refactor: Clean up unused imports and add Kotlin JVM plugin

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -1,4 +1,3 @@
-import com.android.build.api.variant.LibraryAndroidComponentsExtension
 import com.android.build.gradle.LibraryExtension
 import com.instantsystem.newsly.configureKotlinAndroid
 import com.instantsystem.newsly.libs
